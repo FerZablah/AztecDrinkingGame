6dc34e2d7956d811bfa08627ac59819af62f0656
Zablon18 <zablah.developer@gmail.com>
2018-07-25 13:49:38 -0500

    Added card dragging

---
+++ AztecDrinkingGame/Assets/Scripts/CardController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDrag() {
+		Debug.Log("LOG");
+  	float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+  }
+}
