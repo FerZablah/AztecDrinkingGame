bbed1fc05efba8f81d9d746500186820620a60d6
Zablon18 <zablah.developer@gmail.com>
2018-07-31 23:30:10 -0500

    Added Preload Scene

---
+++ AztecDrinkingGame/Assets/Scripts/DDOL.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDOL : MonoBehaviour {
+
+	public void Awake(){
+		DontDestroyOnLoad(gameObject);
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ AztecDrinkingGame/Assets/Scripts/DevPreload.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevPreload : MonoBehaviour {
+
+	void Awake() {
+  	GameObject check = GameObject.Find("__App");
+  	if (check==null){
+			print("entro");
+		 	UnityEngine.SceneManagement.SceneManager.LoadScene("PreLoad");
+		}
+  }
+}
